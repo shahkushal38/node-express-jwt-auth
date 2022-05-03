9a21bf4d14726a9084fb6b118c7bd43c66a68da0
Kushal Shah <60709835+shahkushal38@users.noreply.github.com>
2022-05-03 19:46:52 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+
+// view engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+//mongoose connection
+const dburl = "mongodb+srv://shahkushal38:kushalgindra@clusterjwt.hl1la.mongodb.net/nodejs-jwt-auth?retryWrites=true&w=majority"
+mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+      app.listen(3000)
+    console.log("MongoDB connected")})
+  .catch((err) => console.log(err));
+
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+// app.listen(process.env.PORT || 3000);
+console.log("Server Started at port 3000");
