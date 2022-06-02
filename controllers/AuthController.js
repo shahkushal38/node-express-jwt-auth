3e79f193416a2502eca632f8972810f81ffbab2c
shahkushal38 <shahkushal38@gmail.com>
2022-06-02 14:35:37 +0530

    added user schema

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,25 @@
-module.exports.signup_get=(req,res) => {
-    res.render('signup')
-}
-module.exports.login_get = (req,res) => {
-    res.render('login')
-}
+const User = require("../models/User");
+
+module.exports.signup_get = (req, res) => {
+  res.render("signup");
+};
+module.exports.login_get = (req, res) => {
+  res.render("login");
+};
+module.exports.login_post = (req, res) => {
+  const { email, password } = req.body;
+  console.log("req-- ", email, "pass--- ", password);
+  res.send("Login Request");
+};
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+  console.log("req-- ", email, "pass--- ", password);
+  // res.send("signup request")
+  try {
+    const user = await User.create({ email, password });
+    res.status(200).json(user);
+  } catch (err) {
+    console.log("Error--", err);
+    res.status(400).send("Error in creating user");
+  }
+};
--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const authRoutes = require('./router/AuthRouter')
+const authRoutes = require("./router/AuthRouter");
 
 // middleware
 app.use(express.static("public"));
+app.use(express.json());
 
 // view engine
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
 //mongoose connection
-const dburl = "mongodb+srv://shahkushal38:kushalgindra@clusterjwt.hl1la.mongodb.net/nodejs-jwt-auth?retryWrites=true&w=majority"
-mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true })
+const dburl =
+  "mongodb+srv://shahkushal38:kushalgindra@clusterjwt.hl1la.mongodb.net/nodejs-jwt-auth?retryWrites=true&w=majority";
+mongoose
+  .connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
-      app.listen(3000)
-    console.log("MongoDB connected")})
+    app.listen(3000);
+    console.log("MongoDB connected");
+  })
   .catch((err) => console.log(err));
 
-
 // routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
-app.use(authRoutes)
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
 // app.listen(process.env.PORT || 3000);
 console.log("Server Started at port 3000");
